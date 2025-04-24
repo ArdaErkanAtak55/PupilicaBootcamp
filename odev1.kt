278c16732083dc69239a3933d8d071c8e5928a9b
Arda Erkan Atak <ardaerkanatak@gmail.com>
2025-04-25 01:27:07 +0300

    Odev1

---
+++ odev1.kt
@@ -0,0 +1,56 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "Samsun"
+    var ulke = "Türkiye"
+    val telefon = 5426882693
+    var postaKodu = 55900
+    val email = "ardaerkanatak@gmail.com"
+    var meslek = "Öğrenci"
+    val stokMiktarı = 55555
+    var musteriAdi = "Batuhan"
+    val bakiye = 555.5
+    var dogumGunu = "11 Temmuz 2000"
+    val medeniDurum = "Bekar"
+    var urunYorum = "Son derece iyi"
+    val odemeTarihi = "22.04.2025"
+    var odeme = 550
+    val siparisAdedi = 55
+    var arabaModeli = 2025
+    val kitapAdi = "Böyle Buyurdu Zerdüşt"
+    var yayinlanmaTarihi = 2005
+    var indirimMiktari = 550
+    val odaSayisi = 55
+    var enlem = " 40° 50' - 41° 51' "
+    val boylam = " 37° 08' - 34° 25' "
+    var urunAdi = "Samsun Pidesi"
+    val yemekFiyati = "55₺"
+    var marka = "Elbereth"
+    val muzikAdi = "Bohemian Rhapsody"
+    var videoSuresi = 12.55
+    val urunPuani = 5
+    var resimAdi = "Arda.png"
+    val dosyaFormati = ".kt"
+    var renk = "Sarı"
+    val renkKodu = 5555
+    var telefonModeli = "8 Plus"
+    val ekranBoyutu = " 7' "
+    var agirlik = "55 kg"
+    val ulusalGun = "19 Mayıs"
+    var tatilGunu = "23 Nisan"
+    val rezervasyonTarihi = "18.05.2025"
+    var sokakAdi = "606.Sokak"
+    val otobusHatti = 724
+    var kalanDakika = 12
+    val takipKodu = 555555
+    var kuponSuresi = "12 Gün"
+    val kuponKodu = 5555555
+    var faturaAdresi = "Atatürk Mah/Samsun"
+
+    println("Şehir : $sehir")
+    println("Ağırlık : $agirlik")
+    println("Kalan Dakika : $kalanDakika")
+    println("Fatura Adresi : $faturaAdresi")
+    
+
+}
